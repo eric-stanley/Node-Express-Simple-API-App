a4feaa6e04c505b85c2da41235037ff9ccb0099c
Eric Stanley <ericstanley@pop-os.localdomain>
2020-07-10 14:29:53 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+const feedRoutes = require('./routes/feed');
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+app.listen(process.env.PORT || 3200);
