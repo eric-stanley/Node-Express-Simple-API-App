cb40c4e7071ba8a19ed9d549c529ea5e37c7795b
Eric Stanley <ericstanley@pop-os.localdomain>
2020-07-13 18:20:22 +0530

    Added cors manually

--- app.js
+++ app.js
@@ -22,12 +22,12 @@ const MONGODB_URI = 'mongodb+srv://' + process.env.MONGODB_USERNAME +
 //     '@' + process.env.MONGODB_CLUSTER + '/' +
 //     process.env.MONGODB_DATABASE + '?retryWrites=true&w=majority';
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // app.use(cors({ 
 //   origin: '*',
