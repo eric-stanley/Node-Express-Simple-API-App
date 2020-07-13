2d22718137711ffc2650492c90e60f3b9de41fa5
Eric Stanley <ericstanley@pop-os.localdomain>
2020-07-13 18:35:43 +0530

    Removed server image storage

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ const socket = require('./socket');
 
 app.use(bodyParser.json());
 // app.use('/images', express.static(path.join(__dirname, 'images')));
-
 app.use(fileHelper.upload.single('image'));
 app.use(fileHelper.imageStore.uploadToCloud);
 
